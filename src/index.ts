e1656b69c59ac5af223ee9e7c5f1dfe351c6479e
zync <chandankrishna288@gmail.com>
2024-09-21 17:11:24 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -9,30 +9,28 @@ const io = new Server(server, {
   },
 });
 
+type message = { message: string; formattedTime: string };
+
 io.on("connection", (socket) => {
+  console.log("NewConnection", socket.id.substring(0, 7));
   socket.on("join-room", (room: string) => {
-    // console.log(`${socket.id.substring(0, 7)} joined ${room}`);
+    console.log(`${socket.id.substring(0, 7)} joined ${room}`);
     socket.join(room);
-    socket.on("send-message", (message) => {
-      // console.log(`${socket.id.substring(0, 7)} sent ${message}`);
-      const date = new Date();
-      const formattedTime = date.toLocaleTimeString([], {
-        hour: "2-digit",
-        minute: "2-digit",
-        hour12: true,
-      });
+    socket.on("send-message", (message: message) => {
+      console.log(`${socket.id.substring(0, 7)} sent ${message.message}`);
       io.to(room).emit("receive-message", {
-        message: message,
-        time: formattedTime,
+        message: message.message,
+        time: message.formattedTime,
         sender: socket.id,
       });
     });
   });
+
   socket.on("disconnect", () => {
     console.log(`${socket.id.substring(0, 7)} Disconnected`);
   });
 });
 
 server.listen(3001, () => {
-  console.log("Server is running on port 3001");
+  console.log(`Server is running on port 3001`);
 });
